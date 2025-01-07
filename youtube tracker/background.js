1afc4521bfc5f02d106910f7ca7ce2b9b42236e6
season <sionwer4@naver.com>
2025-01-07 21:49:11 +0900

    feat: Change the track function track only for 1 or 2 minutes

--- youtube tracker/background.js
+++ youtube tracker/background.js
@@ -1,7 +1,8 @@
 let startTime = null;
 let timer = null;
-const TIME_LIMIT = 60 * 1000;
+const TIME_LIMITS = [60 * 1000, 120 * 1000];
 let activeYouTubeTabId = null;
+let notificationsSent = [];
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.url && changeInfo.url.includes("youtube.com")) {
@@ -13,9 +14,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 });
 
-function handleYouTubeStart() {
+function handleYouTubeStart(tabId) {
   if (!startTime) {
+    activeYouTubeTabId = tabId;
     startTime = Date.now();
+    notificationsSent = [];
     console.log("Started tracking YouTube usage!");
     startYouTubeTimer();
   }
@@ -23,8 +26,10 @@ function handleYouTubeStart() {
 
 function handleYouTubeExit() {
   if (startTime) {
-    console.log("Left YouTube, stopping timer.");
     stopYouTubeTimer();
+    activeYouTubeTabId = null;
+    startTime = null;
+    notificationsSent = [];
   }
 }
 
@@ -32,25 +37,32 @@ function startYouTubeTimer() {
   timer = setInterval(() => {
     const elapsedTime = Date.now() - startTime;
 
-    if (elapsedTime >= TIME_LIMIT) {
-      sendNotification();
-      startTime = Date.now();
+    for (let i = 0; i < TIME_LIMITS.length; i++) {
+      if (elapsedTime >= TIME_LIMITS[i] && !notificationsSent.includes(TIME_LIMITS[i])) {
+        sendNotification(i + 1);
+        notificationsSent.push(TIME_LIMITS[i]);
+      }
     }
+
+    if (notificationsSent.length === TIME_LIMITS.length) {
+      console.log("All notifications sent. Stopping timer.");
+      stopYouTubeTimer();
+    }
+
   }, 1000);
 }
 
 function stopYouTubeTimer() {
   clearInterval(timer);
   timer = null;
-  startTime = null;
 }
 
-function sendNotification() {
+function sendNotification(minute) {
   chrome.notifications.create({
     type: "basic",
     iconUrl: "warning.png",
-    title: "YouTube Warning",
-    message: `You have spent 1 minute on YouTube!`,
+    title: "YouTube Usage Tracker Warning",
+    message: `You have spent ${minute} minute${minute > 1 ? "s" : ""} on YouTube!`,
     priority: 1,
   });
 }
