b81d0162df5286164954a8e25ea770d5834599b0
season <sionwer4@naver.com>
2025-01-26 23:01:02 +0900

    feat: Change the timer variable name

--- youtube tracker/background.js
+++ youtube tracker/background.js
@@ -2,7 +2,7 @@ import { loadAccumulatedSpentTimes, recordAccumulatedSpentTimes } from './servic
 import { checkNotificationTimeCondition } from './service/notification.js';
 
 const activeYouTubeTabs = new Set();
-let youTubeTrackerTimer = null;
+let youTubeTimer = null;
 let spentSecond = null;
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
@@ -29,7 +29,7 @@ chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
 function addTabIdToActiveYouTubeTabs(tabId) {
   activeYouTubeTabs.add(tabId);
   console.log("Activated Youtube tab added in management=", tabId);
-  if (youTubeTrackerTimer) {
+  if (youTubeTimer) {
     console.log("The Timer already activated so nothing happened.");
   } else {
     loadAccumulatedSpentTimes().then((startSecond) => {
@@ -43,7 +43,7 @@ function removeTabIdFromActiveYouTubeTabs(tabId) {
   console.log("YouTube tab closed. Remained Youtube tab size=", activeYouTubeTabs.size);
   if (activeYouTubeTabs.size === 0) {
     console.log("There is no more activated Youtube tab. Stopping the timer.");
-    if (youTubeTrackerTimer) {
+    if (youTubeTimer) {
       stopYouTubeTimer();
       recordAccumulatedSpentTimes(spentSecond);
     }
@@ -52,7 +52,7 @@ function removeTabIdFromActiveYouTubeTabs(tabId) {
 
 function startYouTubeTimer(startSecond) {
   spentSecond = startSecond;
-  youTubeTrackerTimer = setInterval(() => {
+  youTubeTimer = setInterval(() => {
     spentSecond++;
     console.log(`${spentSecond} seconds spent on YouTube`);
     checkNotificationTimeCondition(spentSecond);
@@ -60,7 +60,7 @@ function startYouTubeTimer(startSecond) {
 }
 
 function stopYouTubeTimer() {
-  clearInterval(youTubeTrackerTimer);
-  youTubeTrackerTimer = null;
+  clearInterval(youTubeTimer);
+  youTubeTimer = null;
   console.log("Timer stopped.");
 }
